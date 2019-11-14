b61519e3a0f9c540cdc70703661332c1993d68c6
urrik98 <adamrenschen@gmail.com>
2019-11-14 10:24:21 -0800

    ...

--- csv2wiki.rb
+++ csv2wiki.rb
@@ -1,12 +1,11 @@
-#!/usr/bin/env ruby
 
 require 'csv'
 
 csvfilename = ARGV[0]
 outfilename = ARGV[1]
 
-data = File.read(csvfilename)
-csv = CSV.parse(data, :headers => true)
+datafile = File.read(csvfilename)
+csv = CSV.parse(File.read(csvfilename.to_s), :headers => true)
 
 File.open(outfilename,"a+") do |out|
    out.write("{| class='wikitable'\n")
@@ -15,17 +14,18 @@ File.open(outfilename,"a+") do |out|
    csv.headers.each do |col_head|
       head_delimiter = firsthead == true ? "!" : "!!"
       firsthead = false
-      headval = (["",nil].include? col_head) ? " " : col_head
+      headval = (["",nil].include? col_head.to_s) ? " " : col_head.to_s
       wikitablehead += "#{head_delimiter}#{headval}"
    end
    out.write("#{wikitablehead}\n")
    csv.each do |row|
       wikirow = "|"
       firstcell = true
+#puts row
       row.each do |k,v|
          cell_delimiter = firstcell == true ? "" : "||"
          firstcell = false
-         val = (["",nil].include? v) ? " " : v
+         val = (["",nil].include? v.to_s) ? " " : v.to_s
          wikirow += "#{cell_delimiter}#{val}"
       end
       out.write("|- \n#{wikirow}\n")
